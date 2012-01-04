117d48916386d6ab4d614e0de75dfc88cd783779
Olli Huotari <olli.huotari@iki.fi>
2012-01-04 15:13:14 +0200

    using memory database and initializing it in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,5 +10,14 @@ Rails.backtrace_cleaner.remove_silencers!
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
+ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => ':memory:'
 
+silence_stream(STDOUT) do
+  ActiveRecord::Migrator.migrate File.expand_path('../../db/migrate/', __FILE__)
+end
 
+  #def drop_all_tables
+  #  ActiveRecord::Base.connection.tables.each do |table|
+  #    ActiveRecord::Base.connection.drop_table(table)
+  #  end
+  #end
