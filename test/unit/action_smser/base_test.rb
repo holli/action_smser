834b87de45215c4508ccbeba287828bf0a694a70
Olli Huotari <olli.huotari@iki.fi>
2012-01-04 14:15:04 +0200

    test unit in use

--- test/unit/action_smser/base_test.rb
+++ test/unit/action_smser/base_test.rb
@@ -5,10 +5,6 @@ class ActionSmser::BaseTest < ActiveSupport::TestCase
     def basic_sms(to, from, body)
       sms(:to => to, :from => from, :body => body)
     end
-
-    def hello_world()
-      sms(:to => "1234", :from => '1234', :body => 'hello world')
-    end
   end
 
   setup do
@@ -30,118 +26,45 @@ class ActionSmser::BaseTest < ActiveSupport::TestCase
     assert @sms.valid?
   end
 
-  
+  test "should have saved sms_type" do
+    assert_equal "ActionSmser::BaseTest::MockSms.basic_sms", @sms.sms_type
+  end
 
-  #it "should have saved sms_type" do
-  #  assert_equal "MockSms.basic_sms", @sms.sms_type
-  #end
-  #
-  #it "should have copied the delivery_options when initializing" do
-  #  assert @sms.delivery_options == ActionSmser.delivery_options
-  #  assert @sms.delivery_options.object_id != ActionSmser.delivery_options.object_id
-  #end
-  #
-  #describe "delivery" do
-  #
-  #  it "should have test_array as delivery method" do
-  #    assert_equal ActionSmser::DeliveryMethods::TestArray, @sms.delivery_method
-  #  end
-  #
-  #  describe "test_array delivery" do
-  #    before do
-  #      ActionSmser::DeliveryMethods::TestArray.deliveries.clear
-  #      @sms_delivery = @sms.deliver
-  #    end
-  #
-  #    it "should be able to deliver" do
-  #      assert @sms_delivery
-  #    end
-  #
-  #    it "should add to TestArray" do
-  #      assert_equal 1, ActionSmser::DeliveryMethods::TestArray.deliveries.size
-  #      assert_equal @sms, ActionSmser::DeliveryMethods::TestArray.deliveries.first
-  #    end
-  #
-  #  end
-  #
-  #  describe "test_array delivery with saving delivery_reports" do
-  #    before do
-  #      ActionSmser::DeliveryMethods::TestArray.deliveries.clear
-  #      @sms.delivery_options[:save_delivery_reports] = true
-  #      @delivery_reports_count = ActionSmser::DeliveryReport.count
-  #      @sms_delivery = @sms.deliver
-  #    end
-  #
-  #    it "should have created two delivery_reports" do
-  #      assert_equal @delivery_reports_count + 1, ActionSmser::DeliveryReport.count
-  #    end
-  #  end
-  #end
+  test "should have copied the delivery_options when initializing" do
+    assert @sms.delivery_options == ActionSmser.delivery_options
+    assert @sms.delivery_options.object_id != ActionSmser.delivery_options.object_id
+  end
+
+  test "should have test_array as delivery method" do
+    assert_equal ActionSmser::DeliveryMethods::TestArray, @sms.delivery_method
+  end
+
+  ##############################################################
+  ## DELIVERY WITH TEST_ARRAY
+
+  def setup_delivery_with_test_array
+    ActionSmser::DeliveryMethods::TestArray.deliveries.clear
+    @sms_delivery = @sms.deliver
+  end
+
+  test "test_array should be able to deliver" do
+    setup_delivery_with_test_array
+    assert @sms_delivery
+  end
+
+  test "test_array should add to TestArray" do
+    setup_delivery_with_test_array
+    assert_equal 1, ActionSmser::DeliveryMethods::TestArray.deliveries.size
+    assert_equal @sms, ActionSmser::DeliveryMethods::TestArray.deliveries.first
+  end
+
+  test "test_array delivery with saving delivery_reports" do
+    ActionSmser::DeliveryMethods::TestArray.deliveries.clear
+    @sms.delivery_options[:save_delivery_reports] = true
+    @delivery_reports_count = ActionSmser::DeliveryReport.count
+    @sms_delivery = @sms.deliver
+
+    assert_equal @delivery_reports_count + 2, ActionSmser::DeliveryReport.count
+  end
 
 end
-
-
-#describe ActionSmser::Base do
-#  before do
-#
-#  end
-#
-#  it "receivers should be joined by commas" do
-#    @sms.to_encoded.must_equal "555123555,123555123"
-#  end
-#
-#  it "body should be possible to encode" do
-#    @sms.body_encoded_escaped.must_equal "Body+with+%E4%E4kk%F6set+end"
-#  end
-#
-#  it "should be valid sms" do
-#    assert @sms.valid?, @sms
-#  end
-#
-#  it "should have saved sms_type" do
-#    assert_equal "MockSms.basic_sms", @sms.sms_type
-#  end
-#
-#  it "should have copied the delivery_options when initializing" do
-#    assert @sms.delivery_options == ActionSmser.delivery_options
-#    assert @sms.delivery_options.object_id != ActionSmser.delivery_options.object_id
-#  end
-#
-#  describe "delivery" do
-#
-#    it "should have test_array as delivery method" do
-#      assert_equal ActionSmser::DeliveryMethods::TestArray, @sms.delivery_method
-#    end
-#
-#    describe "test_array delivery" do
-#      before do
-#        ActionSmser::DeliveryMethods::TestArray.deliveries.clear
-#        @sms_delivery = @sms.deliver
-#      end
-#
-#      it "should be able to deliver" do
-#        assert @sms_delivery
-#      end
-#
-#      it "should add to TestArray" do
-#        assert_equal 1, ActionSmser::DeliveryMethods::TestArray.deliveries.size
-#        assert_equal @sms, ActionSmser::DeliveryMethods::TestArray.deliveries.first
-#      end
-#
-#    end
-#
-#    describe "test_array delivery with saving delivery_reports" do
-#      before do
-#        ActionSmser::DeliveryMethods::TestArray.deliveries.clear
-#        @sms.delivery_options[:save_delivery_reports] = true
-#        @delivery_reports_count = ActionSmser::DeliveryReport.count
-#        @sms_delivery = @sms.deliver
-#      end
-#
-#      it "should have created two delivery_reports" do
-#        assert_equal @delivery_reports_count + 1, ActionSmser::DeliveryReport.count
-#      end
-#    end
-#  end
-#
-##end
--- test/unit/action_smser/delivery_report_test.rb
+++ test/unit/action_smser/delivery_report_test.rb
@@ -1,49 +1,53 @@
 require 'test_helper'
 
-#require 'minitest/autorun'
-#
-#class MockSms<ActionSmser::Base
-#  def basic_sms(to, from, body)
-#    sms(:to => to, :from => from, :body => body)
-#  end
-#end
-#
-#describe ActionSmser::DeliveryReport do
-#  before do
-#    @receivers = ["555123555", "", "123555123"]
-#    @sender = "555666"
-#    @body = "Body with ääkköset end"
-#    @sms = MockSms.basic_sms(@receivers, @sender, @body)
-#  end
-#
-#  describe "build DeliveryReport from sms" do
-#    before do
-#      @dr = ActionSmser::DeliveryReport.build_from_sms(@sms, "123", "msg_id_a")
-#    end
-#    it "should have set @sms params" do
-#      assert_equal @sender, @dr.from
-#      assert_equal @body, @dr.body
-#      assert_equal @sms.sms_type, @dr.sms_type
-#    end
-#    it "should have set right to info" do
-#      assert_equal "123", @dr.to
-#    end
-#    it "should have set right msg_id" do
-#      assert_equal "msg_id_a", @dr.msg_id
-#    end
-#  end
-#
-#  describe "create_from_save" do
-#    before do
-#      @dr_count = ActionSmser::DeliveryReport.count
-#      @dr = ActionSmser::DeliveryReport.create_from_sms(@sms, "123", "msg_id_a")
-#    end
-#    it "should have created one in db" do
-#      assert_equal @dr_count + 1, ActionSmser::DeliveryReport.count
-#    end
-#    it "should have returned right item" do
-#      assert @dr.is_a?(ActionSmser::DeliveryReport)
-#    end
-#  end
-#
-#end
+class ActionSmser::DeliveryReportTest < ActiveSupport::TestCase
+
+  class MockSms<ActionSmser::Base
+    def basic_sms(to, from, body)
+      sms(:to => to, :from => from, :body => body)
+    end
+  end
+
+  setup do
+    @receivers = ["555123555", "", "123555123"]
+    @sender = "555666"
+    @body = "Body with ääkköset end"
+    @sms = MockSms.basic_sms(@receivers, @sender, @body)
+  end
+
+  test "build DeliveryReport from sms" do
+    @dr = ActionSmser::DeliveryReport.build_from_sms(@sms, "123", "msg_id_a")
+
+    assert @sender == @dr.from, "should have set @sms.from"
+    assert @body == @dr.body, "should have set @sms.body"
+    assert @sms.sms_type == @dr.sms_type, "should have set @sms.sms_type"
+
+    assert "123" == @dr.to, "should have set right to info"
+
+    assert "msg_id_a" == @dr.msg_id, "should have set right msg_id"
+
+  end
+
+  test "create_from_save" do
+    @dr_count = ActionSmser::DeliveryReport.count
+    @dr = ActionSmser::DeliveryReport.create_from_sms(@sms, "123", "msg_id_a")
+
+    assert_equal @dr_count + 1, ActionSmser::DeliveryReport.count, "should have created one in db"
+    assert @dr.is_a?(ActionSmser::DeliveryReport), "should have returned right item"
+  end
+
+
+  test "updating status" do
+    @dr = ActionSmser::DeliveryReport.new
+    @dr.status = "LOCAL_TEST"
+    @dr.save!
+    log = @dr.log
+
+    @dr.status = "TEST_2"
+    assert_equal "TEST_2", @dr.status
+    assert log != @dr.log, "should have updated log"
+    assert @dr.log.include?("TEST_2")
+    assert @dr.save
+  end
+
+end
