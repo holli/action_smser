a654caa7d1e058495c1978c8f1c6bf8658a8e58c
Olli Huotari <olli.huotari@iki.fi>
2012-01-04 16:24:19 +0200

    added navigation tests

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -3,8 +3,9 @@ require 'test_helper'
 class NavigationTest < ActionDispatch::IntegrationTest
   fixtures :all
 
-  # test "the truth" do
-  #   assert true
-  # end
+  test "basic gateway_commit delivery_report" do
+    get 'action_smser/delivery_reports/gateway_commit/example_gateway'
+    assert_response :success
+  end
 end
 
