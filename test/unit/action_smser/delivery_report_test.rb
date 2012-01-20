b383a6f3218e858a0f0e860d58c56c03a0b92850
Olli Huotari <olli.huotari@iki.fi>
2012-01-20 22:24:30 +0200

    delivery_of etc for delivery_Reports

--- test/unit/action_smser/delivery_report_test.rb
+++ test/unit/action_smser/delivery_report_test.rb
@@ -89,7 +89,10 @@ class ActionSmser::DeliveryReportTest < ActiveSupport::TestCase
     assert_equal "#{@sms.sms_type}_resent", @dr_resent.sms_type
 
     assert_equal @dr.id, @dr_resent.re_delivery_of_delivery_report_id, "should set new reports re_delivery_of_delivery_report_id"
+    assert_equal @dr.re_deliveries.first, @dr_resent
+    assert_equal @dr, @dr_resent.re_delivery_of
   end
 
 
 end
+
