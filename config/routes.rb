a0da5d032c1784e3354118aec06c4bbbc1676ee5
Sébastien <synthax.terror@gmail.com>
2013-12-09 16:41:23 +0100

    Rails 4 routes compatibility

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionSmser::Engine.routes.draw do
 
-  match 'delivery_reports/gateway_commit/:gateway' => 'delivery_reports#gateway_commit'
+  match 'delivery_reports/gateway_commit/:gateway' => 'delivery_reports#gateway_commit', via: [:get, :post]
 
   resources :delivery_reports, :only => :index do
     get 'list', :on => :collection
