a2c6595047ea9bead6c1b52d94850f0f50d24720
Olli Huotari <olli.huotari@iki.fi>
2014-07-29 15:08:51 +0300

    schema update

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -8,8 +9,24 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(version: 20120102215215) do
+
+  create_table "action_smser_delivery_reports", force: true do |t|
+    t.string   "msg_id"
+    t.string   "status"
+    t.datetime "status_updated_at"
+    t.string   "sms_type"
+    t.text     "log"
+    t.string   "to"
+    t.string   "from"
+    t.string   "body"
+    t.string   "gateway"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "action_smser_delivery_reports", ["msg_id"], name: "index_action_smser_delivery_reports_on_msg_id"
 
 end
